f78a9a1d7d94b48dc38c444067541b13b5ccb10b
rclank <53981655+rclank@users.noreply.github.com>
2021-11-03 23:46:51 -0400

    UX enhancements

--- assets/js/script.js
+++ assets/js/script.js
@@ -183,7 +183,7 @@ $("#task-form-modal").on("shown.bs.modal", function() {
 });
 
 // save button in modal was clicked
-$("#task-form-modal .btn-primary").click(function() {
+$("#task-form-modal .btn-save").click(function() {
   // get form values
   var taskText = $("#modalTaskDescription").val();
   var taskDate = $("#modalDueDate").val();
@@ -222,16 +222,19 @@ $(".card .list-group").sortable({
   tolerance: "pointer",
   helper: "clone",
   activate: function(event) {
-    // console.log("activate", this);
+    console.log($(this));
+    $(this).addClass('dropover');
+    $('.bottom-trash').addClass('bottom-trash-drag');
   },
   deactivate: function(event) {
-    // console.log("deactivate", this);
+    $(this).removeClass('dropover');
+    $('.bottom-trash').removeClass('bottom-trash-drag');
   },
   over: function(event) {
-    // console.log("over", event.target);
+    $(event.target).addClass('dropover-active');
   },
   out: function(event) {
-    // console.log("out", event.target);
+    $(event.target).removeClass('dropover-active');
   },
   update: function(event) {
     // array to store the task data in
@@ -275,12 +278,13 @@ $("#trash").droppable({
     console.log("drop");
     console.log(ui.draggable);
     ui.draggable.remove();
+    $('.bottom-trash').removeClass('bottom-trash-active');
   },
   over: function(event, ui) {
-    // console.log("over");
+    $('.bottom-trash').addClass('bottom-trash-active');
   },
   out: function(event, ui) {
-    // console.log("out");
+    $('.bottom-trash').removeClass('bottom-trash-active');
   }
 });
 
@@ -288,10 +292,8 @@ $("#modalDueDate").datepicker({
   minDate: 1
 });
 
-
-
-// setInterval(function() {
-//   $(".card .list-group-item").each(function(index, el) {
-//     auditTask(el);
-//   });
-// }, 5000);
+setInterval(function() {
+  $(".card .list-group-item").each(function(index, el) {
+    auditTask(el);
+  });
+}, (1000 * 60) * 30);
